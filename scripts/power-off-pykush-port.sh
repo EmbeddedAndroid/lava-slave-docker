48c80faa4876de52a43e84ad4eb013f904eb8d22
Tyler Baker <forcedinductionz@gmail.com>
2017-05-08 14:39:17 -0500

    scripts: add pykush power off script
    Signed-off-by: Tyler Baker <forcedinductionz@gmail.com>

---
+++ scripts/power-off-pykush-port.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# $1 - ykush ID: YK23310  retrieve using pykush -l
+# $2 - device id: {1,2,3}
+
+flock /var/lock/lava-pykush.lck pykush -s $1 -d $2
+while [ $? -ne 0 ]; do
+	echo "Disable port failed, retrying..."
+	flock /var/lock/lava-pykush.lck pykush -s $1 -d $2
+done
