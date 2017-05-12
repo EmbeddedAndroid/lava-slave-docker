aa55dd10c80a5f4f723314403c51b1763c8e7126
Tyler Baker <tyler.baker@linaro.org>
2017-05-12 15:04:01 -0700

    scripts: flock: use serial number for flock calls
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- scripts/power-off-pykush-port.sh
+++ scripts/power-off-pykush-port.sh
@@ -2,8 +2,8 @@
 # $1 - ykush ID: YK23310  retrieve using pykush -l
 # $2 - device id: {1,2,3}
 
-flock /var/lock/lava-pykush.lck pykush -s $1 -d $2
+flock /var/lock/lava-pykush-$1.lck pykush -s $1 -d $2
 while [ $? -ne 0 ]; do
 	echo "Disable port failed, retrying..."
-	flock /var/lock/lava-pykush.lck pykush -s $1 -d $2
+	flock /var/lock/lava-pykush-$1.lck pykush -s $1 -d $2
 done
