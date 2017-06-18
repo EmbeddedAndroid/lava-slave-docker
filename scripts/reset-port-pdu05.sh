4d5fd71c0809e3b469ae9661da1014b03f088bf9
Tyler Baker <tyler.baker@linaro.org>
2017-06-17 20:40:18 -0700

    scripts/reset-port-pdu05: refactor sleep
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- scripts/reset-port-pdu05.sh
+++ scripts/reset-port-pdu05.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /root/scripts/pdu05.exp 0 $1
-sleep 2
-/root/scripts/pdu05.exp 1 $1
 if [[ -n "$2" ]]; then
-	echo "Sleeping..."
 	sleep $2
+else
+	sleep 2
 fi
+/root/scripts/pdu05.exp 1 $1
